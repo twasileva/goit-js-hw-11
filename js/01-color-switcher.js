431d2638392cff5d16ecac69fe180f10a68e1982
[twasileva] <[twasileva@gmail.com]>
2021-07-15 22:03:48 +0300

    done 01-color-switcher

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,25 @@
+refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]')
+}
+let timerId = null
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+refs.startBtn.addEventListener('click', onChangeColorBody)
+refs.stopBtn.addEventListener('click', onCancelColorBody)
+
+function onChangeColorBody() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+}
+
+function onCancelColorBody() {
+  clearInterval(timerId);
+  document.body.style.backgroundColor = ''
+}
