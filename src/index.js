5ac17de9d2be3d90866f45a733ea0f613b519a68
[twasileva] <[twasileva@gmail.com]>
2021-07-26 21:42:05 +0300

    timer in the process

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
 import './sass/main.scss';
-import './js/01-color-switcher'
-import './js/02-timer'
-import './js/03-promisify'
---
+++ src/js/02-timer.js
@@ -0,0 +1,53 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  inputDate: document.getElementById('date-selector'),
+  dataDays: document.querySelector('span[data-days]'),
+  dataHours: document.querySelector('span[data-hours]'),
+  dataMinutes: document.querySelector('span[data-minutes]'),
+  dataSeconds: document.querySelector('span[data-seconds]')
+}
+class Timer {
+  constructor() {
+
+  }
+
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now()
+      // console.log(currentTime);
+    }, 1000);
+
+  }
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+
+}
+
+const timer = new Timer()
+refs.startBtn.addEventListener('click', timer.start.bind(timer))
+
+refs.inputDate.addEventListener('blur', e => console.log(Date.parse(new Date(e.target.value))))
+
+
+
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
